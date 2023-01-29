76f59d54c213b71306d74188e22fd0d6373ef53b
gerardoMundo <ger.mundo66@gmail.com>
2023-01-28 21:37:53 -0600

    actividad 1.3.

--- formulario/main.js
+++ formulario/main.js
@@ -1,6 +1,8 @@
+const content = document.getElementById("content");
+const form = document.getElementById("form");
+
 const formValidation = () => {
   const inputs = document.querySelectorAll("form [required]");
-
   inputs.forEach((input) => {
     const span = document.createElement("span");
     span.id = input.name;
@@ -13,7 +15,7 @@ const formValidation = () => {
     if (e.target.matches(".form-contact [required]")) {
       const input = e.target,
         pattern = input.pattern || input.dataset.pattern;
-      console.log(input);
+
       if (pattern) {
         let regex = new RegExp(pattern);
         return !regex.exec(input.value)
@@ -30,4 +32,33 @@ const formValidation = () => {
     }
   });
 };
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let dataForm = new FormData(form); // valores del formulario
+
+  const insertDataRows = (dataForm) => {
+    let table = document.getElementById("table"); // tabla donde se renderiza la data
+    let newRow = table.insertRow(-1);
+
+    let newNameCell = newRow.insertCell(0);
+    newNameCell.textContent = dataForm.get("nombre");
+
+    let newLastNameCell = newRow.insertCell(1);
+    newLastNameCell.textContent = dataForm.get("apellido");
+
+    let newEmailCell = newRow.insertCell(2);
+    newEmailCell.textContent = dataForm.get("correo");
+
+    let newOptionsCell = newRow.insertCell(3);
+    newOptionsCell.textContent = dataForm.get("opciones");
+
+    let newGenderCell = newRow.insertCell(4);
+    newGenderCell.textContent = dataForm.get("genero");
+  };
+
+  insertDataRows(dataForm);
+});
+
 formValidation();
